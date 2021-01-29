7e2319178429de4af8eb5c38d627e95f820f42b9
Shami Tomita <shamitomita@gmail.com>
2021-01-29 23:05:12 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+class Doctor 
+  attr_accessor :name
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+  
+  def appointments  
+    Appointments.all.select {|app| app.doctor == self}
+  end 
+  
+  def patients
+    
+  
+end 
