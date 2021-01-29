795bde20fd5c382891bd9a5c05e98ea599c76417
Shami Tomita <shamitomita@gmail.com>
2021-01-29 23:13:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end 
   
   def appointments  
-    Appointments.all.select {|app| app.doctor == self}
+    Appointment.all.select {|app| app.doctor == self}
   end 
   
   def patients
